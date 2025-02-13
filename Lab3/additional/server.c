eca8b04cb069cc59550ac30d8697e42cdd9d7f4a
adi673 <codingadityawadgaonkar@gmail.com>
2025-02-13 06:29:21 +0530

    l3 additional

--- Lab3/additional/server.c
+++ Lab3/additional/server.c
@@ -8,103 +8,124 @@
 #define SOCKET_PATH "/tmp/calc_socket"
 #define MAXSIZE 1024
 
-void handle_request(int client_sock) {
+void handle_request(int client_sock)
+{
     int option;
     char buffer[MAXSIZE];
 
-    while (1) {
+    while (1)
+    {
         // Receive the option from the client
         int n = recv(client_sock, &option, sizeof(int), 0);
-        if (n <= 0) {
+        if (n <= 0)
+        {
             printf("Client disconnected.\n");
             break;
         }
 
         // Process the request
-        switch (option) {
-            case 1: { // Addition & Subtraction
-                int a, b, sum, diff;
-                recv(client_sock, &a, sizeof(int), 0);
-                recv(client_sock, &b, sizeof(int), 0);
-                sum = a + b;
-                diff = a - b;
-                snprintf(buffer, sizeof(buffer), "Sum: %d, Difference: %d", sum, diff);
-                send(client_sock, buffer, strlen(buffer), 0);
-                break;
+        switch (option)
+        {
+        case 1:
+        { // Addition & Subtraction
+            int a, b, sum, diff;
+            recv(client_sock, &a, sizeof(int), 0);
+            recv(client_sock, &b, sizeof(int), 0);
+            sum = a + b;
+            diff = a - b;
+            snprintf(buffer, sizeof(buffer), "Sum: %d, Difference: %d", sum, diff);
+            send(client_sock, buffer, strlen(buffer), 0);
+            break;
+        }
+
+        case 2:
+        { // Solve linear equation (ax + b = 0)
+            int a, b;
+            double x;
+            recv(client_sock, &a, sizeof(int), 0);
+            recv(client_sock, &b, sizeof(int), 0);
+
+            memset(buffer, 0, sizeof(buffer)); // ✅ Clear buffer before use
+
+            if (a == 0)
+            {
+                snprintf(buffer, sizeof(buffer), "Invalid equation (a cannot be 0)");
+            }
+            else
+            {
+                x = -(double)b / a;
+                snprintf(buffer, sizeof(buffer), "x = %.2f", x);
             }
 
-            case 2: { // Solve linear equation (ax + b = 0)
-                int a, b;
-                double x;
-                recv(client_sock, &a, sizeof(int), 0);
-                recv(client_sock, &b, sizeof(int), 0);
-                if (a == 0) {
-                    strcpy(buffer, "Invalid equation (a cannot be 0)");
-                } else {
-                    x = -(double)b / a;
-                    snprintf(buffer, sizeof(buffer), "x = %.2f", x);
-                }
-                send(client_sock, buffer, strlen(buffer), 0);
-                break;
-            }
+            send(client_sock, buffer, strlen(buffer), 0);
+            break;
+        }
 
-            case 3: { // Matrix Multiplication
-                int m1, n1, m2, n2;
-                recv(client_sock, &m1, sizeof(int), 0);
-                recv(client_sock, &n1, sizeof(int), 0);
-                int mat1[m1][n1];
+        case 3:
+        { // Matrix Multiplication
+            int m1, n1, m2, n2;
+            recv(client_sock, &m1, sizeof(int), 0);
+            recv(client_sock, &n1, sizeof(int), 0);
+            int mat1[m1][n1];
+            for (int i = 0; i < m1; i++)
+                recv(client_sock, mat1[i], n1 * sizeof(int), 0);
+
+            recv(client_sock, &m2, sizeof(int), 0);
+            recv(client_sock, &n2, sizeof(int), 0);
+            int mat2[m2][n2];
+            for (int i = 0; i < m2; i++)
+                recv(client_sock, mat2[i], n2 * sizeof(int), 0);
+
+            if (n1 != m2)
+            {
+                strcpy(buffer, "Matrix multiplication not possible.");
+            }
+            else
+            {
+                int res[m1][n2];
+                memset(res, 0, sizeof(res));
                 for (int i = 0; i < m1; i++)
-                    recv(client_sock, mat1[i], n1 * sizeof(int), 0);
+                    for (int j = 0; j < n2; j++)
+                        for (int k = 0; k < n1; k++)
+                            res[i][j] += mat1[i][k] * mat2[k][j];
 
-                recv(client_sock, &m2, sizeof(int), 0);
-                recv(client_sock, &n2, sizeof(int), 0);
-                int mat2[m2][n2];
-                for (int i = 0; i < m2; i++)
-                    recv(client_sock, mat2[i], n2 * sizeof(int), 0);
-
-                if (n1 != m2) {
-                    strcpy(buffer, "Matrix multiplication not possible.");
-                } else {
-                    int res[m1][n2];
-                    memset(res, 0, sizeof(res));
-                    for (int i = 0; i < m1; i++)
-                        for (int j = 0; j < n2; j++)
-                            for (int k = 0; k < n1; k++)
-                                res[i][j] += mat1[i][k] * mat2[k][j];
-
-                    snprintf(buffer, sizeof(buffer), "Resultant Matrix:\n");
-                    for (int i = 0; i < m1; i++) {
-                        for (int j = 0; j < n2; j++) {
-                            char temp[10];
-                            snprintf(temp, sizeof(temp), "%d ", res[i][j]);
-                            strcat(buffer, temp);
-                        }
-                        strcat(buffer, "\n");
+                snprintf(buffer, sizeof(buffer), "Resultant Matrix:\n");
+                for (int i = 0; i < m1; i++)
+                {
+                    for (int j = 0; j < n2; j++)
+                    {
+                        char temp[10];
+                        snprintf(temp, sizeof(temp), "%d ", res[i][j]);
+                        strcat(buffer, temp);
                     }
+                    strcat(buffer, "\n");
                 }
-                send(client_sock, buffer, strlen(buffer), 0);
-                break;
             }
+            send(client_sock, buffer, strlen(buffer), 0);
+            break;
+        }
 
-            case 4:
-                printf("Client requested exit.\n");
-                return;
+        case 4:
+            printf("Client requested exit.\n");
+            return;
 
-            default:
-                strcpy(buffer, "Invalid option.");
-                send(client_sock, buffer, strlen(buffer), 0);
+        default:
+            strcpy(buffer, "Invalid option.");
+            send(client_sock, buffer, strlen(buffer), 0);
         }
     }
 }
 
-int main() {
+int main()
+{
     int server_sock, client_sock;
     struct sockaddr_un server_addr, client_addr;
     socklen_t client_len;
 
     // Create socket
     server_sock = socket(AF_UNIX, SOCK_STREAM, 0);
-    if (server_sock == -1) {
+    if (server_sock == -1)
+    {
         perror("Socket creation failed");
         exit(1);
     }
@@ -115,13 +136,15 @@ int main() {
     server_addr.sun_family = AF_UNIX;
     strncpy(server_addr.sun_path, SOCKET_PATH, sizeof(server_addr.sun_path) - 1);
 
-    if (bind(server_sock, (struct sockaddr *)&server_addr, sizeof(server_addr)) == -1) {
+    if (bind(server_sock, (struct sockaddr *)&server_addr, sizeof(server_addr)) == -1)
+    {
         perror("Bind failed");
         close(server_sock);
         exit(1);
     }
 
-    if (listen(server_sock, 5) == -1) {
+    if (listen(server_sock, 5) == -1)
+    {
         perror("Listen failed");
         close(server_sock);
         exit(1);
@@ -129,10 +152,12 @@ int main() {
 
     printf("Server waiting for connections...\n");
 
-    while (1) {
+    while (1)
+    {
         client_len = sizeof(client_addr);
         client_sock = accept(server_sock, (struct sockaddr *)&client_addr, &client_len);
-        if (client_sock == -1) {
+        if (client_sock == -1)
+        {
             perror("Accept failed");
             continue;
         }
